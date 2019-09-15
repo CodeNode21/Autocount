b55b6717d0363dfbc925f3082d40161ed6f1d70f
CodeNode21 <mbednara@gmail.com>
2019-09-15 00:10:28 -0400

    removed some files

---
+++ src/components/jumbotron.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './styles.css';
+
+
+function Jumbotron(){
+    return (
+        <div className="jumbotron jumbotron-fluid">
+            <div className="container">
+                <h1 className="display-4">Autocount+</h1>
+                <p className="lead">Automated Statement Reconciliation</p>
+            </div>
+        </div>
+    );
+}
+
+export default Jumbotron;
